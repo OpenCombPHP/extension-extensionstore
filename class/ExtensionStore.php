bb19f4adfe2f1132930a77cfcbb1c01cc1079caa
kongyuan <anubiskong@gmail.com>
2012-04-12 15:07:40 +0800

    first commit

---
+++ class/ExtensionStore.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\extensionstore;
+
+use org\jecat\framework\lang\aop\AOP;
+use org\opencomb\platform\ext\Extension ;
+
+class ExtensionStore extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+	}
+}
